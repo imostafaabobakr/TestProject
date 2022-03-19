b03432b4b5b2718d24e44af9c59ef8cba872c20d
Mostafa Abobakr <mostafa_e.b.b@hotmail.com>
2022-03-20 00:46:15 +0200

    v1.0

---
+++ index.js
@@ -0,0 +1,27 @@
+const Joi = require('joi');
+const express = require('express');
+const req = require('express/lib/request');
+const res = require('express/lib/response');
+const { compileQueryParser } = require('express/lib/utils');
+const app = express();
+
+app.use(express.json());
+
+const courses = [
+    {id: 1, name: 'robotics'},
+    {id: 2, name:'compilers'},
+    {id: 3, name : 'cryptography'},
+    {id: 4, name : 'test'}
+];
+
+app.get('/', (req,res)=>{
+res.send('helloworld');
+});
+app.get('/api/courses',(req,res)=> {
+  res.send(courses);  
+
+});
+const port = process.env.port || 3000;
+app.listen(port,()=>console.log(`listening on port ${port}....`));
+// functions
+

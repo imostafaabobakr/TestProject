42775d928b73a841d88efaae292e470eec3c65d7
Mostafa Abobakr <mostafa_e.b.b@hotmail.com>
2022-04-27 01:54:47 +0200

    fix error

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const {courseRouter} = require('./routes/course')
+const {bookRouter} = require('./routes/book')
+
 const app = express()
 
 app.use(express.json())
@@ -7,5 +9,7 @@ app.use(express.json())
 
 
 app.use("/courses/", courseRouter)
+app.use("/books/", bookRouter)
+
 
 module.exports = {app};

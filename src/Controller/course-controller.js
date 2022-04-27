c910a8f0d7318bcb26f65f2c894d032905b03ede
Mostafa Ramzy <47761317+mostafaramzyabdelganey@users.noreply.github.com>
2022-04-27 02:34:34 +0200

    getCourses

--- src/Controller/course-controller.js
+++ src/Controller/course-controller.js
@@ -10,4 +10,25 @@ const createCourse = async (req, res) =>{
         res.status(400).send(e)
      }
 }
+const getCourses = async (req, res) =>{
+    try{
+        const courses = await Course.find({})
+        res.status(200).send(courses)
+    }
+    catch(e){
+       res.status(400).send(e)
+    }
+}
+
+const getCourseById = async (req, res) =>{
+
+    try{
+        const courseId =  req.params.id
+        const course = await Course.findById({_id : courseId})
+        res.status(200).send(course)
+    }
+    catch(e){
+        res.status(400).send(e)
+     }
+}
 

86ad654f295ee39333649807286dbea1fb595bda
muhammedezzat <48224880+muhammedezzat@users.noreply.github.com>
2022-04-27 02:37:35 +0200

    updated app.js to resolve conflict

--- src/app.js
+++ src/app.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const {courseRouter} = require('./routes/course')
-const {reviewRouter} = require('./routes/review')
 const app = express()
+
+
+
+const {reviewRouter} = require('./routes/review')
+
 
 app.use(express.json())
 
 
 
 app.use("/courses/", courseRouter)
+
+
+
+
 app.use("/reviews/", reviewRouter)
+
 module.exports = {app};

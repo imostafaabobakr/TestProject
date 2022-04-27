8f6fa9b3228ec01eff230286dd07dd19ca2ba6ac
Skyho1d <47832007+Skyho1d@users.noreply.github.com>
2022-04-27 04:46:08 +0200

    get and getbyId

--- src/controllers/student-controller.js
+++ src/controllers/student-controller.js
@@ -10,3 +10,25 @@ const createStudent = async (req, res) =>{
         res.status(400).send(e)
      }
 }
+
+const getStudents = async (req, res) =>{
+    try{
+        const students = await Student.find({})
+        res.status(200).send(students)
+    }
+    catch(e){
+       res.status(400).send(e)
+    }
+}
+
+const getStudentById = async (req, res) =>{
+
+    try{
+        const studentId =  req.params.id
+        const student = await Student.findById(studentId).populate({ path: 'course', select: 'name' });
+        res.status(200).send(student)
+    }
+    catch(e){
+        res.status(400).send(e)
+     }
+}

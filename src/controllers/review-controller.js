6a43d7fe09854702660a8bfa18a9347c07cb7b13
muhammedezzat <48224880+muhammedezzat@users.noreply.github.com>
2022-04-27 03:50:03 +0200

    add update review

--- src/controllers/review-controller.js
+++ src/controllers/review-controller.js
@@ -32,3 +32,36 @@ const getReviewById = async (req, res) =>{
         res.status(400).send(e)
      }
 }
+
+const updateReview = async (req,res) =>{
+    try{
+        const reviewId =  req.params.id
+        // const review = await review.findByIdAndUpdate({_id : reviewId}, req.body, {new:true});
+        const review = await Review.findOne({_id : reviewId})
+
+        if(req.body.student){
+            review.student = req.body.student;
+        }
+        if(req.body.postedOn){
+            review.postedOn = req.body.postedOn;
+        }
+
+        if(req.body.rating){
+            review.rating = req.body.rating;
+        }
+    
+
+        if(req.body.studnumber){
+            review.review = req.body.review;
+        }
+    
+    
+
+        await review.save()
+        res.status(200).send(review)
+    }
+    catch(e){
+        res.status(400).send(e)
+     }
+
+}

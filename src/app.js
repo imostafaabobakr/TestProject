c1afefb7eea275e989bb241b84a2f472fadfa26c
muhammedezzat <48224880+muhammedezzat@users.noreply.github.com>
2022-04-27 02:33:22 +0200

    added review model and fixed app.js
    I forget to edit app.js in the previous commit

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const {courseRouter} = require('./routes/course')
+const {reviewRouter} = require('./routes/review')
 const app = express()
 
 app.use(express.json())
@@ -7,5 +8,5 @@ app.use(express.json())
 
 
 app.use("/courses/", courseRouter)
-
+app.use("/reviews/", reviewRouter)
 module.exports = {app};
